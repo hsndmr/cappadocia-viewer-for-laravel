2f9ee0ed6eb06f1d7c62d800b7207ce0f88a756c
Hasan Demir <hsndmr5044@hotmail.com>
2023-07-31 03:16:42 +0300

    feat: update configuration for CappadociaViewer with server details and options

--- config/cappadocia-viewer.php
+++ config/cappadocia-viewer.php
@@ -1,6 +1,9 @@
 <?php
 
-// config for Hsndmr/CappadociaViewer
-return [
+declare(strict_types=1);
 
+return [
+    'server_url' => env('CAPPADOCIA_VIEWER_SERVER_URL', 'http://127.0.0.1:9091'),
+    'timeout'    => env('CAPPADOCIA_VIEWER_TIMEOUT', 3),
+    'enabled'    => env('CAPPADOCIA_VIEWER_ENABLED', true),
 ];
