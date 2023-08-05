705be50898eb907a381cb8739a638543ab45b1dd
Hasan Demir <hsndmr5044@hotmail.com>
2023-08-05 16:31:12 +0300

    test: refactor HelpersTest to use CappadociaViewer

--- tests/HelpersTest.php
+++ tests/HelpersTest.php
@@ -1,10 +1,11 @@
 <?php
 
 declare(strict_types=1);
-use Hsndmr\CappadociaViewer\Builders\SenderViewerBuilder;
 
-it('returns a new instance of SenderViewerBuilder', function (): void {
+use Hsndmr\CappadociaViewer\CappadociaViewer;
+
+it('returns a new instance of CappadociaViewer', function (): void {
     $senderViewerBuilder = cappadocia();
 
-    expect($senderViewerBuilder)->toBeInstanceOf(SenderViewerBuilder::class);
+    expect($senderViewerBuilder)->toBeInstanceOf(CappadociaViewer::class);
 });
