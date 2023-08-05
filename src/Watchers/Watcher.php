62ef41b9fd47d0160373c054a2791dfb48f8f174
Hasan Demir <hsndmr5044@hotmail.com>
2023-08-05 05:13:44 +0300

    feat: extend Watcher class with methods to control watching state

--- src/Watchers/Watcher.php
+++ src/Watchers/Watcher.php
@@ -6,5 +6,36 @@ namespace Hsndmr\CappadociaViewer\Watchers;
 
 abstract class Watcher
 {
+    protected bool $isWatching = false;
+
     abstract public function register(): void;
+
+    abstract protected function getConfigName(): string;
+
+    public function isWatching(): bool
+    {
+        if (config('cappadocia-viewer.watch_'.$this->getConfigName(), false)) {
+            return true;
+        }
+
+        if ($this->isWatching) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function watch(): self
+    {
+        $this->isWatching = true;
+
+        return $this;
+    }
+
+    public function stopWatching(): self
+    {
+        $this->isWatching = false;
+
+        return $this;
+    }
 }
