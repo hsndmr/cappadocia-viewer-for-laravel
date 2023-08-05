db7e9046f665d810f853f25d22b4026aed78cfd4
Hasan Demir <hsndmr5044@hotmail.com>
2023-08-05 03:54:47 +0300

    feat: add QUERY case to ViewerType enum

--- src/Enums/ViewerType.php
+++ src/Enums/ViewerType.php
@@ -8,12 +8,14 @@ enum ViewerType
 {
     case LOG;
     case JOB;
+    case QUERY;
 
     public function type(): string
     {
         return match ($this) {
-            self::LOG => 'log',
-            self::JOB => 'job',
+            self::LOG   => 'log',
+            self::JOB   => 'job',
+            self::QUERY => 'query',
         };
     }
 }
